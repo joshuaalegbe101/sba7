543e6c1d963fe76d6fdf0d668fc67482db88720e
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-02-02 21:23:46 -0600

    Edits

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
-const express = require("express");
+const express = require("require");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+mongoose.connect(process.env.ATLAS_URI)
+mongoose.connection.on("connection", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}!`)
+});
+
+app.use(express.urlencoded({ extended: false}));
+
+//ROUTES
+
+
+
+
+
+
+
+
+//PORT STUFF
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log("Server Listening On Port: ", PORT)
+});
